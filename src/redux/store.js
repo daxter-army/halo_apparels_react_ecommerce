9e590fdb492775ec3e4a4e62e08c9d3c1a34359a
mehul <mehulsaini456@gmail.com>
2020-07-22 01:38:18 +0530

    added redux-persist to maintain info in browser's localstorage to prevent cart items to get lost on page reload

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux';
 
-import rootReducer from './root-reducer'
+import { persistStore } from 'redux-persist';
 
-const middlewares = [logger]
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+import rootReducer from './root-reducer';
 
-export default store
+const middlewares = [logger];
+
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export const persistor = persistStore(store);
