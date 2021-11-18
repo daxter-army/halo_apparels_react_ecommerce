06c92a0668b3b537a1d5caa316dd15eaba926f16
Mehul Singh Teya <mehulsaini456@gmail.com>
2021-11-18 20:12:31 +0530

    refactored footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './footer.styles.scss'
 
 const Footer = () => (
-    <div className="footer">Made with <span role='img' aria-label='heart'>❤️</span> by Mehul Singh Teya. Visit <a href="https://www.github.com/daxter-army/halo_apparels_react_ecommerce" target="_blank" rel="noopener noreferrer">Github Repository</a></div>
+    // <div className="footer">Made with <span role='img' aria-label='heart'>❤️</span> by Mehul Singh Teya. Visit <a href="https://www.github.com/daxter-army/halo_apparels_react_ecommerce" target="_blank" rel="noopener noreferrer">Github Repository</a></div>
+    <div className="footer">Made with <span role='img' aria-label='heart'>❤️</span></div>
 );
 
 export default Footer;
